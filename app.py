5942403201d6b4f8decede55364c58155314d5f0
johansmunoz <91511832+johansmunoz@users.noreply.github.com>
2025-04-14 19:50:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -112,7 +112,7 @@ def update_heatmap(selected_group):
     data = df_filtered[group1] if selected_group == "group1" else df_filtered[group2]
     title = f"📊 Monthly Correlation Heatmap ({selected_group.replace('group', 'Group ')})"
     return plot_heatmap(data, title)
-
+    
 # Run the server locally
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
